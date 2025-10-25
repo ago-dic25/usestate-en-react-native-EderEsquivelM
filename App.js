78016bcff521a258b4ca575fe7102b0918434c7d
Eder-Esquivel-LP <ederesquivel142@gmail.com>
2025-10-25 10:46:53 -0600

    Ejercicio completo

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { estiloTextos } from './misEstilos';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [nombre, setNombre] = useState('');
+
+  const obtenerEmoji = () => {
+    if (nombre.length === 0) return '🤔';
+    if (nombre.length < 10) return '🙂';
+    if (nombre.length < 15) return '😄';
+    return '🤩';
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text style={estiloTextos.texto}>Hola, mundo</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>Captura tu nombre:</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Escribe tu nombre"
+        placeholderTextColor="#87a1ab"
+        value={nombre}
+        onChangeText={setNombre}
+      />
+
+      <Text style={styles.texto}>
+        Tu nombre es: <Text style={styles.resaltado}>{nombre || '---'}</Text>
+      </Text>
+
+      <Text style={styles.texto}>
+        Caracteres: {nombre.replace(/\s/g, '').length} {obtenerEmoji()}
+      </Text>
+
+      <TouchableOpacity style={styles.boton} onPress={() => setNombre('')}>
+        <Text style={styles.textoBoton}>Limpiar 🧹</Text>
+      </TouchableOpacity>
+
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -15,8 +44,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#c8dfe2',
     alignItems: 'center',
     justifyContent: 'center',
-  }
+  },
+  titulo: {
+    color: '#547071', 
+    fontSize: 22,
+    marginBottom: 15,
+    fontWeight: 'bold',
+  },
+  texto: {
+    color: '#547071',
+    fontSize: 16,
+  },
+  resaltado: {
+    color: '#a7bcf7', 
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#87a1ab',
+    color: '#fff',
+    borderRadius: 10,
+    padding: 10,
+    width: '80%',
+    marginBottom: 10,
+  },
+  boton: {
+    backgroundColor: '#688391',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    marginTop: 15,
+  },
+  textoBoton: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
